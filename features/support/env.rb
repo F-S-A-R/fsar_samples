5f2fdd6a43e6950155f6516b5c80ebe02bc37a3c
Rich Downie <rich@blackboxd.com>
2020-11-08 07:34:05 -0500

    update env rb with docker localhost conditional

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,19 +2,23 @@ require 'rspec' #for page.shoud etc
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'pry'
-
+require 'webdrivers'
 #get IP of host which has 4444 mapped from other container
 docker_ip = %x(/sbin/ip route|awk '/default/ { print $3 }').strip
 
-Capybara.register_driver :remote_chrome do |app|
+Capybara.register_driver :docker_chrome do |app|
   Capybara::Selenium::Driver.new(app,
-  :browser => :remote,
-  :desired_capabilities => :chrome,
-  :url => "http://#{docker_ip}:4444/wd/hub")
+                                 :browser => :remote,
+                                 :desired_capabilities => :chrome,
+                                 :url => "http://#{docker_ip}:4444/wd/hub")
 end
 
 Capybara.configure do |config|
   config.run_server = false
-  config.default_driver = :remote_chrome
+  if docker_ip.empty?
+    config.default_driver = :selenium_chrome
+  else
+    config.default_driver = :docker_chrome
+  end
   config.app_host = 'http://localhost:3000' # change this to point to your application
 end
